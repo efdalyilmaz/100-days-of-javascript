e5a129ccbed3202eaec88ed446b9bfa82a642ca9
efdalyilmaz <yilmazefdal@gmail.com>
2022-09-01 18:18:21 +0300

    commit test

--- random-hex-color-generator/script.js
+++ random-hex-color-generator/script.js
@@ -1,10 +1,12 @@
-const color = document.querySelector(".color");
+const hex = document.querySelector(".hex");
 const btn = document.querySelector(".generate");
+
 
 const generateColor = () => {
     const rand = Math.random().toString(16).substring(2,8);
-    color.innerHTML = "#"+rand;
-    document.body.style.backgroundColor =color.innerHTML;
+    hex.innerHTML = "#"+rand;
+    document.body.style.backgroundColor =hex.innerHTML;
 }
+
 
 btn.addEventListener("click", generateColor);
